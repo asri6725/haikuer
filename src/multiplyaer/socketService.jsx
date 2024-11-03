86ca500ade0f46c5be602d2d7f4deff6f1534483
asri6725 <abhinandan.srinivas@gmail.com>
2024-11-03 15:26:05 +1100

    point to ngrok tunnel

--- src/multiplyaer/socketService.jsx
+++ src/multiplyaer/socketService.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://13.236.193.140:443');
+const socket = io('https://4725-13-236-193-140.ngrok-free.app');
 
 export function initializeSocket() {
   socket.connect();
